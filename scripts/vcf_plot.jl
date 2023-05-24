c38062bb4a46b85ad8614810cbe05395f03e0c38
NessInMorse <itsok2001@gmail.com>
2023-05-24 21:05:17 +0200

    Added DataFrames to the vcf_plot script

--- scripts/vcf_plot.jl
+++ scripts/vcf_plot.jl
@@ -4,7 +4,7 @@ VCF PLOT
 A script that creates an interactive plot of the data in the vcf-file
     containing all the SNP-mutation types per contig in a heatmap
 
-VERSION: 0.1
+VERSION: 0.2 - Added DataFrames package
 Date: 2023-05-24
 Author: Marc Wijnands
 
@@ -14,6 +14,18 @@ Was also not able to test functionality yet.
 
 Planned to let it create plots using Plotly(JS)
 =#
+try
+        using DataFrames
+        using PlotlyJS
+catch
+        using Pkg
+        Pkg.add("DataFrames")
+        Pkg.add("PlotlyJS")
+finally
+        using DataFrames
+        using PlotlyJS
+end
+
 
 function returnIndex(base::SubString{String})
     #=
@@ -35,7 +47,7 @@ function returnIndex(base::SubString{String})
 end
 
 
-function readFile(infile::IOStream, titles::Vector{String})::Dict{String, Vector{Vector{Int}}}
+function readFile(infile::IOStream, titles::Vector{String})::Dict{String, Matrix{Int}}
     #=
     Reads a VCf-file and adds the counts to a Dictionary containing all the SNPs per contig.
     N and structural variants <*> are deleted.
@@ -45,15 +57,14 @@ function readFile(infile::IOStream, titles::Vector{String})::Dict{String, Vector
     out:
             The dictionary containing the SNP counts per contig
     =#
-    all_items::Dict{String, Vector{Vector{Int}}} = Dict()
-    
+    all_items::Dict{String, Matrix{Int}} = Dict() 
     items::Vector{SubString{String}} = []
     alt::Vector{SubString{String}} = []
     for line::String in eachline(infile)
             if !(line[1] == '#')
                     items = split(line, "\t")
                     contig = items[1]
-                    if !(get(all_items, contig, true)) # if the contig can not be found in the contig, add said contig to dictionary
+                    if get(all_items, contig, 0) == 0 # if the contig can not be found in the contig, add said contig to dictionary
                         all_items[contig] = zeros(Int32, (4, 4))
                     end
                     ref = items[4]
@@ -70,22 +81,31 @@ function readFile(infile::IOStream, titles::Vector{String})::Dict{String, Vector
                                     end
                             end
                     end
-
             end
     end
     close(infile)
-    return scores
+    return all_items
 end
 
 
 function main()
     if length(ARGS) == 2
         infile = open(ARGS[1], "r")
-        outfile = open(ARGS[2], "w")
+        # outfile = open(ARGS[2], "w")
         titles::Vector{String} = ["A", "C", "G", "T"] # We remember this sequence
-        scores = readFile(infile, titles)
-        close(infile)
-        close(outfile)
+        all_contigs = readFile(infile, titles)
+        all_dfs::Dict{String, DataFrame} = Dict()
+        for key in keys(all_contigs)
+                df = DataFrame(all_contigs[key], :auto)
+                rename!(df, combo)
+                df[!, "combination"] = combo
+                
+                all_dfs[key] = df
+        end
+        
+        # println(all_dfs)
+        # println(length(all_dfs))
+        # close(outfile)
     end
 end
 
